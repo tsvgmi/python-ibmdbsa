b08863de7dab0f24fd2bdb6cfe3b9b693ef5c6bb
tvuong <thien.vuong@etrade.com>
2016-05-10 15:26:56 -0700

    Bump version

--- ibm_db_sa/ibm_db_sa/__init__.py
+++ ibm_db_sa/ibm_db_sa/__init__.py
@@ -17,7 +17,7 @@
 # | Contributors: Jaimy Azle, Mike Bayer                                     |
 # +--------------------------------------------------------------------------+
 
-__version__ = '0.3.2'
+__version__ = '0.3.2.1'
 
 from . import ibm_db, pyodbc, base, zxjdbc
 
